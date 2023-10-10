3127ec2a6270aff988d66b44b3038697439b756e
Mihuy1 <patrik.hyytiainen@protonmail.com>
2023-10-10 12:44:25 +0300

    Cleanup some comments in code.

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
          database='flight_game',
-         user='',
+         user='root',
          password='',
          autocommit=True
          )
