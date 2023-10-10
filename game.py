090dcff171d9c9b7a5428765e473fd905c2af0fe
ArmasNev <armas.n82@gmail.com>
2023-10-10 14:35:14 +0300

    game loop changed

--- game.py
+++ game.py
@@ -12,9 +12,11 @@ connection = mysql.connector.connect(
          password='',
          autocommit=True
          )
+money = 10000
+p_range = money*4
 
 def get_airports():
-    sql = """SELECT iso_country, ident, name, type
+    sql = """SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
     FROM airport
     WHERE type='large_airport'
     ORDER by RAND()
@@ -76,11 +78,18 @@ def get_airport_info(icao):
 
 def airport_distance(current, target):
     start = get_airport_info(current)[0]  # Access the first (and only) item in the list
-    print(start)
     end = get_airport_info(target)[0]  # Access the first (and only) item in the list
     start_coords = (start['latitude_deg'], start['longitude_deg'])
     end_coords = (end['latitude_deg'], end['longitude_deg'])
     return distance.distance(start_coords, end_coords).km
+
+def airports_in_range(icao, a_ports, p_range):
+    in_range = []
+    for a_port in a_ports:
+        dist = airport_distance(icao, a_port['ident'])
+        if dist <= p_range and not dist == 0:
+            in_range.append(a_port)
+    return in_range
 
 def check_event(g_id, cur_airport):
     sql = f'''SELECT events.id, event.id as event_id, events.game_id
@@ -96,11 +105,10 @@ def check_event(g_id, cur_airport):
     return result
 
 
-def update_location(icao, g_id, time, money):
-    sql = f'''UPDATE game SET location = %, name = %, time = %, bank = % '''
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute(sql, (icao, g_id, time, money))
-
+def update_location(g_id, name, icao, m, time):
+    sql = '''UPDATE game SET name = %s, location = %s,  bank = %s, time = %s  WHERE id = %s'''
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql, (name, icao, money, time, g_id))
 
 
 storyDialog = input('Do you want to read the background story? (Y/N): ')
@@ -131,7 +139,7 @@ score = 0
 pet_found = False
 
 # all airports
-airports = get_airports()
+all_airports = get_airports()
 # start_airport ident
 s_airport = airports[0]['ident']
 
@@ -142,16 +150,40 @@ game_id = new_game(player, s_airport, t_limit, money, airports)
 
 # GAME LOOP
 while not game_over:
-    # get current airport info
-    airport = get_airport_info(current_airport)
-    # show game status
-    print(f'''You are at {airport[0]['name']}.''')
-    print(f'''You have {money:.0f}$ and {t_limit} hours left to find a {pet}.''')
-    # pause
-    input('\033[32mPress Enter to continue...\033[0m')
-    # if airport has goal ask if player wants to open it
-    # check goal type and add/subtract money accordingly
     event = check_event(game_id, current_airport)
+    airports = airports_in_range(current_airport, all_airports, p_range)
+    print(f'''\033[34mThere are {len(airports)} airports in range: \033[0m''')
+    if len(airports) == 0:
+        print('You are out of range.')
+        game_over = True
+    while not game_over:
+        # get current airport info
+        airport = get_airport_info(current_airport)
+        # show game status
+        print(f"You are at {airport[0]['ident']} ({airport[0]['name']}).")
+        print(f'''You have {money:.0f}$ and {t_limit} hours left to find a {pet}.''')
+        print('Your pet is in one of these airports:')
+
+        for i in range(len(all_airports)):
+            ap_distance = airport_distance(current_airport, all_airports[i]["ident"])
+            print(
+                f'{i + 1}. {all_airports[i]["name"]}, icao: {all_airports[i]["ident"]}, distance: {ap_distance:.0f}km)')
+        if p_range < 0:
+            game_over = True
+            # ask for destination
+        dest = input("Where do you want to go? ICAO:")
+        selected_distance = airport_distance(current_airport, dest)
+        money -= selected_distance/4
+        update_location(game_id, player, dest, money, t_limit)
+        current_airport = dest
+        if p_range < 0:
+            game_over = True
+    # if pet is found and player is at start, game is won
+    if win and current_airport == s_airport:
+        print(f'You are at {airport[0]["name"]}.')
+        print(f'You have {money:.0f}$ and {t_limit} hours left to find a {pet}.')
+        game_over = True
+
 
 
 
